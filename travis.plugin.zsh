4e74d961a21f38659af95a949f303346c34e85d2
MenkeTechnologies <linux.dev25@gmail.com>
2020-05-12 19:17:20 -0400

    aliases to branch/build/pr

--- travis.plugin.zsh
+++ travis.plugin.zsh
@@ -19,21 +19,22 @@ function __trav_common_url(){
   url="${url/git:\/\//}"
   url="${url/.com/}"
   url="${url/.git/}"
+  url="$url$1"
 }
 
 function __trav_get_url() {
-    __trav_common_url
+    __trav_common_url "$@"
   url="https://travis-ci.org/$url"
 }
 
 function __trav_get_priv_url() {
-    __trav_common_url
+    __trav_common_url "$@"
     url="https://travis-ci.com/$url"
 }
 
 function trav-git() {
   if __trav_check_yml; then
-    __trav_get_url
+    __trav_get_url "$@"
     __trav_open $url
   else
     echo "No .travis.yml file found." >&2
@@ -42,11 +43,26 @@ function trav-git() {
 
 function trav-priv-git() {
   if __trav_check_yml; then
-    __trav_get_priv_url
+    __trav_get_priv_url "$@"
     __trav_open $url
   else
     echo "No .travis.yml file found." >&2
   fi
 }
 
+function trav-priv-git-br() {
+    trav-priv-git "/branches"
+}
+
+function trav-priv-git-pr() {
+    trav-priv-git "/pull_requests"
+}
+
+function trav-priv-git-build() {
+    trav-priv-git "/builds"
+}
+
 alias tg=trav-priv-git
+alias tgb=trav-priv-git-build
+alias tgbr=trav-priv-git-br
+alias tgpr=trav-priv-git-pr
