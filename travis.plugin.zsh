5c230b91a53c82fc0d7f90cde789de77c6cc2da7
MenkeTechnologies <linux.dev25@gmail.com>
2020-05-13 04:10:19 -0400

    when not part of zpwr

--- travis.plugin.zsh
+++ travis.plugin.zsh
@@ -1,4 +1,34 @@
-#!/bin/zsh
+(( $+functions[getOpenCommand] )) ||
+getOpenCommand(){
+    local open_cmd
+
+    case "$ZPWR_OS_TYPE" in
+        darwin*)  open_cmd='open'
+          ;;
+        cygwin*)  open_cmd='cygstart'
+          ;;
+        linux*)
+            if [[ "$(uname -r)" != *icrosoft* ]];then
+                open_cmd='nohup xdg-open'
+            else
+                open_cmd='cmd.exe /c start ""'
+            fi
+            ;;
+        msys*)    open_cmd='start ""'
+            ;;
+        *)        echo "Platform $ZPWR_OS_TYPE not supported"
+                    return 1
+                    ;;
+    esac
+
+    echo "$open_cmd"
+}
+
+if [[ -z "$ZPWR_OPEN_CMD" ]]; then
+    ZPWR_OPEN_CMD="$(getOpenCommand)"
+fi
+
+
 function __trav_open() {
 
     ${=ZPWR_OPEN_CMD} "$1"
