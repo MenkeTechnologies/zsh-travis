ba03f591b114d2fed816115c1a994413a516fca5
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-05 20:06:16 -0400

    autoload

--- travis.plugin.zsh
+++ travis.plugin.zsh
@@ -29,63 +29,14 @@ if [[ -z "$ZPWR_OPEN_CMD" ]]; then
 fi
 
 
-function __trav_open() {
-
-    ${=ZPWR_OPEN_CMD} "$1" 2>/dev/null
-}
-
-function __trav_check_yml() {
-  trav_file="$(git rev-parse --show-toplevel)/.travis.yml"
-  test -f $trav_file || return 1
-}
-
-function __trav_common_url(){
-
-  repo_url=$(git config --get remote.origin.url)
-  branch=$(git rev-parse --abbrev-ref HEAD)
-  url="${repo_url/https:\/\//}"
-  url="${url/http:\/\//}"
-  url="${url/ssh:\/\//}"
-  url="${url/git:\/\//}"
-  url="${url/.com/}"
-  url="${url/.git/}"
-  url="$url$1"
-}
-
-function __trav_get_url() {
-    __trav_common_url "$@"
-    tempurl="https://travis-ci.com/$url"
-    if curl -s --head  --request GET $tempurl | grep "404 Not Found" > /dev/null;then
-        finalurl="https://travis-ci.org/$url"
-    else
-        finalurl="$tempurl"
-
-    fi
-
-}
-
-function trav-git() {
-  if __trav_check_yml; then
-    __trav_get_url "$@"
-    __trav_open $finalurl
-  else
-    echo "No .travis.yml file found." >&2
-  fi
-}
-
-function trav-git-br() {
-    trav-git "/branches"
-}
-
-function trav-git-pr() {
-    trav-git "/pull_requests"
-}
-
-function trav-git-build() {
-    trav-git "/builds"
-}
-
 alias tg=trav-git
 alias tb=trav-git-build
 alias tbr=trav-git-br
 alias tpr=trav-git-pr
+
+0="${${0:#$ZSH_ARGZERO}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# util fns
+fpath+=("${0:h}/autoload")
+autoload -Uz "${0:h}/autoload/"*(.:t)
